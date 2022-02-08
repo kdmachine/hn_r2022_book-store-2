c020ca7a4adfd6707d999479307336ba6799d8d4
dkmachine <kdstudy21@gmail.com>
2022-02-08 07:10:30 +0700

    Template website for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope "(:locale)", locale: /en|vi/ do
+    root "admin_pages#home"
+    get "admin_pages/home"
+    get "/category", to: "admin_pages#category"
+    get "/product", to: "admin_pages#product"
+    get "/order", to: "admin_pages#order"
+    get "/account", to: "admin_pages#account"
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    get "/signup", to: "users#new"
+    post "/signup", to: "users#create"
+  end
 end
